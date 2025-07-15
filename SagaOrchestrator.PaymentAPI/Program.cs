77968a17f15bb4ed0e2034689c45d4c2fad73937
ebranquinho <branquinhomanu@gmail.com>
2025-07-15 07:34:25 -0300

    add payment's endpoints to receive and compensate

--- SagaOrchestrator.PaymentAPI/Program.cs
+++ SagaOrchestrator.PaymentAPI/Program.cs
@@ -1,13 +1,12 @@
+using SagaOrchestrator.Common.Contracts;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -16,29 +15,19 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// TODO save to database
+var captured = new List<Guid>();
 
-app.MapGet("/weatherforecast", () =>
+app.MapPost("/payment", (CapturePayment cmd) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    captured.Add(cmd.OrderId);
+    return Results.Ok(new Success(cmd.OrderId));
+});
 
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+app.MapPost("/payment/refund", (Guid orderId) =>
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+    captured.Remove(orderId);
+    return Results.Ok();
+});
+
+await app.RunAsync();
